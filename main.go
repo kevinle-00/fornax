65e2d6b899562f626c71606ef094b5803c54976a
Kevin Le <kevinle252@gmail.com>
2026-02-24 00:31:16 +1100

    feat(download): add youtube video downloading via yt-dlp

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("fornax")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: fornax <url>")
+		os.Exit(1)
+	}
+
+	url := os.Args[1]
+	err := download(url)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+}
+
+func download(url string) error {
+	cmd := exec.Command("yt-dlp", url)
+	// Piping the yt-dlp's standard output/errors to our terminal
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
