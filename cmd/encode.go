afbea3f2683f29fd58bb1bff7795d4a2ff8ed248
Kevin Le <kevinle252@gmail.com>
2026-02-26 20:17:46 +1100

    refactor(encode): extracted encoding logic into internal/encode package

---
+++ cmd/encode.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/kevinle-00/fornax/internal/encode"
+	"github.com/spf13/cobra"
+)
+
+var encodeCmd = &cobra.Command{
+	Use:   "encode <input> <output>",
+	Short: "Encode media file format",
+	Args:  cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		// TODO: Add validation
+		inputPath := args[0]
+		outputPath := args[1]
+
+		// TODO: Make encode manually cancellable by user
+		encoder := encode.New()
+		err := encoder.Encode(context.Background(), inputPath, outputPath)
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(encodeCmd)
+}
---
+++ internal/encode/encode.go
@@ -0,0 +1,25 @@
+// Package encode is a ffmpeg wrapper
+package encode
+
+import (
+	"context"
+	"os"
+	"os/exec"
+)
+
+type Encoder interface {
+	Encode(ctx context.Context, inputPath, outputPath string) error
+}
+
+type FFmpeg struct{} //
+
+func New() *FFmpeg {
+	return &FFmpeg{}
+}
+
+func (f *FFmpeg) Encode(ctx context.Context, inputPath, outputPath string) error {
+	cmd := exec.CommandContext(ctx, "ffmpeg", "-i", inputPath, outputPath)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
